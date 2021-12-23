64ea17774e4b635413704a25696538a3aec9d9ee
Andre Gibel <GdNCoder@users.noreply.github.com>
2021-12-23 16:16:02 +0100

    Add files via upload

---
+++ RunOptimizationToolOnVM.ps1
@@ -0,0 +1,3 @@
+Set-Location "C:\AVDOptimizationTool"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+.\Win10_VirtualDesktop_Optimize.ps1 -AcceptEULA:$true -Restart:$true -Verbose
